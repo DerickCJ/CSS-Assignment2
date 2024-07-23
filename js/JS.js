dd5e52ed267a5665baea36800e1ea71151a818f3
DerickCJ <xiaoshenqian@gmail.com>
2024-07-23 01:29:31 -0400

    2

--- js/JS.js
+++ js/JS.js
@@ -7,3 +7,42 @@ $(window).on("scroll", function () {
     $(".mask").removeClass("scrolledMask");
   }
 });
+
+
+// Slide up
+function SlideUp() {
+  var reveals = document.querySelectorAll(".slide-up");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", SlideUp);
+
+
+
+
+
+// Slide right
+function SlideRight() {
+  var reveals = document.querySelectorAll(".slide-right");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", SlideRight);
